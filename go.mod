55b82e288ff8aa33b60b3986eed1486dd9f2406a
Bruno Rodrigues <eibrunorodrigues@gmail.com>
2021-01-04 15:24:26 -0300

    orchestrator included

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module github.com/eibrunorodrigues/infra-grpc
 go 1.15
 
 require (
-	github.com/eibrunorodrigues/rabbitmq-go v0.0.0-20201230225947-1feb4b5bb83e
+	github.com/eibrunorodrigues/rabbitmq-go v0.0.0-20201231001538-ebc3a4761c49
 	github.com/golang/protobuf v1.4.2
 	github.com/segmentio/ksuid v1.0.3
 	google.golang.org/grpc v1.34.0
 )
 
-// replace github.com/eibrunorodrigues/rabbitmq-go => ../github.com/eibrunorodrigues/rabbitmq-go
+replace github.com/eibrunorodrigues/rabbitmq-go => ../github.com/eibrunorodrigues/rabbitmq-go
--- main.go
+++ main.go
@@ -1,24 +1,49 @@
 package main
 
 import (
+	context "context"
 	"fmt"
+	"github.com/eibrunorodrigues/infra-grpc/orchestrator"
 	"log"
 	"net"
 	"strings"
 
 	"github.com/eibrunorodrigues/infra-grpc/receiver"
 	"github.com/eibrunorodrigues/infra-grpc/sender"
+	"github.com/eibrunorodrigues/rabbitmq-go/enums"
 	"github.com/eibrunorodrigues/rabbitmq-go/rabbitmq"
 	"github.com/eibrunorodrigues/rabbitmq-go/types"
 	"github.com/segmentio/ksuid"
 	"google.golang.org/grpc"
 )
 
+//Orchestrator struct
+type Orchestrator struct {
+	Client       *rabbitmq.Client
+	instantiated bool
+}
+
+//Publisher struct
 type Publisher struct {
 	Client       *rabbitmq.Client
 	instantiated bool
 }
 
+//Receiver struct
+type Receiver struct {
+	Client       *rabbitmq.Client
+	instantiated bool
+}
+
+//New creates a new instance of a Receiver
+func (r *Receiver) New() {
+	client := rabbitmq.Client{}
+	r.Client = &client
+	r.Client.Connect()
+	r.instantiated = true
+}
+
+//New creates a new instance of a Publisher
 func (p *Publisher) New() {
 	client := rabbitmq.Client{}
 	p.Client = &client
@@ -26,16 +51,154 @@ func (p *Publisher) New() {
 	p.instantiated = true
 }
 
-type Receiver struct {
-	Client       *rabbitmq.Client
-	instantiated bool
+//New creates a new instance of a Orchestrator
+func (o *Orchestrator) New() {
+	client := rabbitmq.Client{}
+	o.Client = &client
+	o.Client.Connect()
+	o.instantiated = true
 }
 
-func (r *Receiver) New() {
-	client := rabbitmq.Client{}
-	r.Client = &client
-	r.Client.Connect()
-	r.instantiated = true
+//AcknowledgeMessage
+func (o *Orchestrator) AcknowledgeMessage(ctx context.Context, arg *orchestrator.MessageId) (*orchestrator.Status, error) {
+	if !o.instantiated {
+		o.New()
+	}
+
+	err := o.Client.AcknowledgeMessage(int(arg.MessageId))
+
+	if err != nil {
+		return &orchestrator.Status{Status: false}, err
+	}
+
+	return &orchestrator.Status{Status: true}, nil
+}
+
+//RejectMessage
+func (o *Orchestrator) RejectMessage(ctx context.Context, arg *orchestrator.Reject) (*orchestrator.Status, error) {
+	if !o.instantiated {
+		o.New()
+	}
+
+	err := o.Client.RejectMessage(int(arg.Id.MessageId), arg.Requeue)
+
+	if err != nil {
+		return &orchestrator.Status{Status: false}, err
+	}
+
+	return &orchestrator.Status{Status: true}, nil
+}
+
+//CheckIfQueueExists
+func (o *Orchestrator) CheckIfQueueExists(ctx context.Context, arg *orchestrator.Name) (*orchestrator.Status, error) {
+	if !o.instantiated {
+		o.New()
+	}
+
+	return &orchestrator.Status{Status: o.Client.CheckIfQueueExists(arg.Name)}, nil
+}
+
+//CheckIfRouterExists
+func (o *Orchestrator) CheckIfRouterExists(ctx context.Context, arg *orchestrator.Name) (*orchestrator.Status, error) {
+	if !o.instantiated {
+		o.New()
+	}
+
+	return &orchestrator.Status{Status: o.Client.CheckIfRouterExists(arg.Name)}, nil
+}
+
+//CreateQueue
+func (o *Orchestrator) CreateQueue(ctx context.Context, arg *orchestrator.QueueCreation) (*orchestrator.Name, error) {
+	if !o.instantiated {
+		o.New()
+	}
+
+	queueName, err := o.Client.CreateQueue(arg.QueueName, arg.CreateDlq, arg.IsAnExclusive)
+	if err != nil {
+		return &orchestrator.Name{}, err
+	}
+
+	return &orchestrator.Name{Name: queueName}, nil
+}
+
+//CreateRouter
+func (o *Orchestrator) CreateRouter(ctx context.Context, arg *orchestrator.RouterCreation) (*orchestrator.Name, error) {
+	if !o.instantiated {
+		o.New()
+	}
+
+	routerPrefix, err := enums.ParseRouterPrefix(strings.Replace(arg.Prefix.String(), "P_", "", -1))
+	if err != nil {
+		return &orchestrator.Name{}, err
+	}
+
+	routerType, err := enums.ParseRouterType(strings.Replace(arg.RouterType.String(), "T_", "", -1))
+	if err != nil {
+		return &orchestrator.Name{}, err
+	}
+
+	routerName, err := o.Client.CreateRouter(arg.RouterName, routerPrefix, routerType)
+	if err != nil {
+		return &orchestrator.Name{}, err
+	}
+
+	return &orchestrator.Name{Name: routerName}, nil
+}
+
+//DeleteQueue
+func (o *Orchestrator) DeleteQueue(ctx context.Context, arg *orchestrator.Name) (*orchestrator.Status, error) {
+	if !o.instantiated {
+		o.New()
+	}
+
+	status, err := o.Client.DeleteQueue(arg.Name)
+	if err != nil {
+		return &orchestrator.Status{}, err
+	}
+
+	return &orchestrator.Status{Status: status}, nil
+}
+
+//DeleteRouter
+func (o *Orchestrator) DeleteRouter(ctx context.Context, arg *orchestrator.Name) (*orchestrator.Status, error) {
+	if !o.instantiated {
+		o.New()
+	}
+
+	status, err := o.Client.DeleteRouter(arg.Name)
+	if err != nil {
+		return &orchestrator.Status{}, err
+	}
+
+	return &orchestrator.Status{Status: status}, nil
+}
+
+//BindQueueToRouter
+func (o *Orchestrator) BindQueueToRouter(ctx context.Context, arg *orchestrator.Bind) (*orchestrator.Status, error) {
+	if !o.instantiated {
+		o.New()
+	}
+
+	status, err := o.Client.BindQueueToRouter(arg.Source, arg.Destination, arg.Filters)
+	if err != nil {
+		return &orchestrator.Status{}, err
+	}
+
+	return &orchestrator.Status{Status: status}, nil
+}
+
+//BindRouterToRouter
+func (o *Orchestrator) BindRouterToRouter(ctx context.Context, arg *orchestrator.Bind) (*orchestrator.Status, error) {
+	if !o.instantiated {
+		o.New()
+	}
+
+	status, err := o.Client.BindRouterToRouter(arg.Source, arg.Destination, arg.Filters)
+	if err != nil {
+		return &orchestrator.Status{}, err
+	}
+
+	return &orchestrator.Status{Status: status}, nil
 }
 
 //Publish allows you to publish a message to RabbitMQ
@@ -63,7 +226,7 @@ func (p *Publisher) Publish(server sender.Sender_PublishServer) error {
 	}
 }
 
-//Receive(*ReceiverArgs, Receiver_ReceiveServer) error
+//Receive (*ReceiverArgs, Receiver_ReceiveServer) error
 func (r *Receiver) Receive(args *receiver.ReceiverArgs, server receiver.Receiver_ReceiveServer) error {
 	if !r.instantiated {
 		r.New()
@@ -109,6 +272,7 @@ func mapToFilters(items map[string]string) []types.Filters {
 
 	return filters
 }
+
 func filtersToMapString(filters []types.Filters) map[string]string {
 	table := make(map[string]string)
 	for _, item := range filters {
@@ -127,8 +291,10 @@ func main() {
 
 	var publisher Publisher
 	var receptor Receiver
+	var orchestrate Orchestrator
 	sender.RegisterSenderServer(grpcServer, &publisher)
 	receiver.RegisterReceiverServer(grpcServer, &receptor)
+	orchestrator.RegisterOrchestratorServer(grpcServer, &orchestrate)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve GRPC 5052: %v", err)
